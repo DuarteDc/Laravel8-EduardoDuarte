8c2d466a18777e8961b0ed9c5ac1ebc1eaafb7c3
DuarteDc <al221910409@gmail.com>
2021-03-02 01:36:23 -0600

    uno a uno

---
+++ database/migrations/2021_03_02_071429_create_coches_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCochesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('coches', function (Blueprint $table) {
+            $table->id();
+            $table->string('modelo');
+            $table->string('color');
+            $table->integer('año');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('coches');
+    }
+}
---
+++ database/migrations/2021_03_02_071637_create_empleados_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmpleadosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('empleados', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('apellidos');
+            $table->string('direccion');
+            $table->string('ciudad');
+            $table->string('estado');
+            $table->unsignedBigInteger('coche_id');
+            $table->foreign('coche_id')->references('id')->on('coches');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('empleados');
+    }
+}
