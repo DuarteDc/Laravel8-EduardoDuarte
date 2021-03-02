ea963a2c39a4df20024d9d08cc4b9ad68090d179
DuarteDc <al221910409@gmail.com>
2021-03-02 01:06:23 -0600

    muchos a muchos

---
+++ database/migrations/2021_03_02_064624_create_prestamos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePrestamosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prestamos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('estudiante_id');
+            $table->unsignedBigInteger('libro_id');
+            $table->foreign('estudiante_id')->references('id')->on('estudiantes');
+            $table->foreign('libro_id')->references('id')->on('libros');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prestamos');
+    }
+}
